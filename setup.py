01c08bc8438b2f6529f335f9c437664dc24dd097
albertkjoller <albert_k_jacobsen@hotmail.com>
2024-05-04 23:04:32 +0200

    Setup repo

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='transformer_redundancy',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Research project (Cognitive Spaces)',
+    author='Albert Kjøller Jacobsen',
+    license='MIT',
+)
